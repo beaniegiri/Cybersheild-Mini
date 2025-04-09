e3642b4b54bccf26b242a08a3d096518d7ddea3f
beaniegiri <binisagiri0@gmail.com>
2025-04-08 22:50:57 -0400

    update

--- Cybersheild-Mini/text_sniffer.py
+++ Cybersheild-Mini/text_sniffer.py
@@ -1,41 +1,55 @@
 import json
+from transformers import pipeline
+
+sentiment_analyzer=pipeline("sentiment-analysis")
 
 # Step 1: Load abusive words from file
 def load_abusive_words(file_path):
     with open(file_path, 'r') as file:
         return [word.strip().lower() for word in file.readlines()]
+    
+#Step2: Analyze sentiment of the input text
+def analyze_sentiment(text):
+        result=sentiment_analyzer(text)[0]
+        return{
+            "sentiment":result['label'],
+            "confidence":round(result['score'],3)}
 
-# Step 2: Check text for abusive words
+# Step 3: Check text for abusive words
 def detect_abuse(text, abusive_words):
     detected = []
     words_in_text = text.lower().split()  # Split input text into words
-    
+    sentiment_report=analyze_sentiment(text)
     for word in abusive_words:
         if word in words_in_text:
             detected.append({
                 "word": word,
                 "severity": "high", 
-                "text_analyzed": text 
+                "text_analyzed": text,
+                "sentiment":sentiment_report
                   # Move the text analyze into the dictionary itself. #implement sentiment analysis librays, # share the offensive model repo
-
+   
             })
+    #sentiment_report=analyze_sentiment(text)
     if detected:
         return{
-            'abusive-words-found':detected
+            'abusive-words-found':detected,
+            #'sentiment':sentiment_report
         }
     else:
         return{
             "abusive-words_found":[],
+            'sentiment':sentiment_report, #this one 
             "text_analyze":text
         }
     # return {
     #     "abusive_words_found": detected,
     #     #"text_analyzed": text,
         
-        
+   #sentiment_report=analyze_sentiment(text)     
     
 
-# Step 3: Main function (runs when script is executed)
+# Step 4: Main function (runs when script is executed)
 if __name__ == "__main__":
     # Load abusive words
     abusive_words = load_abusive_words("abusive_words.txt")
@@ -51,3 +65,6 @@ if __name__ == "__main__":
         json.dump(report, f, indent=4)  # `indent=4` makes JSON readable
     
     print("Analysis complete! Report saved to 'abuse_report.json'.")
+
+    
+    
